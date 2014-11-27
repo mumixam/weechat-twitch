8261546b96a28add6e4098abc1a190fca9c5eb8f
mumixam <mumixam@gmail.com>
2014-11-27 04:32:44 -0600

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -15,12 +15,15 @@
 #
 #
 # This script checks stream status of any channel on the 'twitch' server
-# when you switch to its buffer or by issuing /twitch in the buffer
+# when you switch to its buffer and displays it in topic/title bar.
+# It also displays stream title in channel when change is detected.
+# Force status update by issuing /twitch in buffer
 
 import weechat
 import json
 import datetime
 import time
+import string
 
 SCRIPT_NAME = "twitch"
 SCRIPT_AUTHOR = "mumixam"
@@ -45,6 +48,8 @@ def days_hours_minutes(td):
 
 
 def twitch_main(data, buffer, args):
+    if not args == 'bs':
+        weechat.buffer_set(buffer, 'localvar_set_tstatus', '')
     username = weechat.buffer_get_string(buffer, 'short_name').replace('#', '')
     server = weechat.buffer_get_string(buffer, 'localvar_server')
     type = weechat.buffer_get_string(buffer, 'localvar_type')
@@ -52,19 +57,39 @@ def twitch_main(data, buffer, args):
         return weechat.WEECHAT_RC_OK
     url = 'https://api.twitch.tv/kraken/streams/' + username
     url_hook_process = weechat.hook_process(
-        "url:%s" % url, 5 * 1000, "process_complete", buffer)
+        "curl %s" % url, 7 * 1000, "process_complete", buffer)
     return weechat.WEECHAT_RC_OK
+
+
+gamelist = [
+    "Counter-Strike: Global Offensive;CSGO",
+    "World of Warcraft: Warlords of Draenor;WOW"
+    ]
+
+
+def gameshort(game):
+    global gamelist
+    for games in gamelist:
+        gamelong=games.split(';')[0]
+        if gamelong.lower() == game.lower():
+            return('<'+games.split(';')[-1]+'>')
+        else:
+            return '<'+game+'>'
+
+cleantitle = lambda title: ''.join(filter(string.printable.__contains__, title))
 
 
 def process_complete(data, command, rc, stdout, stderr):
     try:
         jsonDict = json.loads(stdout.strip())
-    except:
+    except Exception as e: 
         weechat.prnt(data, 'TWITCH: Error with twitch API')
         return weechat.WEECHAT_RC_OK
     currentbuf = weechat.current_buffer()
     title_fg=weechat.color(weechat.config_color(weechat.config_get("weechat.bar.title.color_fg")))
     title_bg=weechat.color(weechat.config_color(weechat.config_get("weechat.bar.title.color_bg")))
+    pcolor=weechat.color('chat_prefix_network')
+    ccolor=weechat.color('chat')
     red=weechat.color('red')
     blue=weechat.color('blue')
     green=weechat.color('green')
@@ -72,17 +97,39 @@ def process_complete(data, command, rc, stdout, stderr):
     if not jsonDict['stream']:
         weechat.buffer_set(data, "title", "STREAM: %sOFFLINE%s %sCHECKED AT: %s" % (red, title_fg, blue, ptime))
     else:
-        createtime = jsonDict['stream']['created_at']
-        viewers = jsonDict['stream']['viewers']
-        followers = jsonDict['stream']['channel']['followers']
-        starttime = datetime.datetime.fromtimestamp(
-            time.mktime(time.strptime(createtime + " GMT", "%Y-%m-%dT%H:%M:%SZ %Z")))
         currenttime = datetime.datetime.utcnow()
-        dur = currenttime - starttime
-        uptime = days_hours_minutes(dur)
-
-        weechat.buffer_set(data, "title", "STREAM: %sLIVE%s with %s viewers started %s ago [%s followers] %sCHECKED AT: %s" % (
-            green, title_fg, viewers, uptime, followers, blue, ptime))
+        output='STREAM: %sLIVE%s' % (green, title_fg)
+        if 'game' in jsonDict['stream']:
+            game = gameshort(jsonDict['stream']['game'])
+            output += ' %s with' % game
+        if 'viewers' in jsonDict['stream']:
+            viewers = jsonDict['stream']['viewers']
+            output += ' %s viewers started' % viewers
+        if 'created_at' in jsonDict['stream']:
+            createtime = jsonDict['stream']['created_at']
+            starttime = datetime.datetime.fromtimestamp(
+                time.mktime(time.strptime(createtime + " GMT", "%Y-%m-%dT%H:%M:%SZ %Z")))
+            dur = currenttime - starttime
+            uptime = days_hours_minutes(dur)
+            output += ' %s ago' % uptime
+        if 'channel' in jsonDict['stream']:
+            if 'followers' in jsonDict['stream']['channel']:
+                followers = jsonDict['stream']['channel']['followers']
+                output += ' [%s followers]' % followers
+            if 'status' in jsonDict['stream']['channel']:
+                title = cleantitle(jsonDict['stream']['channel']['status'])
+                oldtitle = weechat.buffer_get_string(data, 'localvar_tstatus')
+                if not oldtitle == title:
+                    weechat.buffer_set(data, 'localvar_set_tstatus', title)
+                    weechat.prnt(data, '%s--%s Title is "%s"' % (pcolor, ccolor, title))
+            if 'updated_at' in jsonDict['stream']['channel']:
+                updateat = jsonDict['stream']['channel']['updated_at'] 
+                updatetime = datetime.datetime.fromtimestamp(
+                    time.mktime(time.strptime(updateat + " GMT", "%Y-%m-%dT%H:%M:%SZ %Z")))
+                udur = currenttime - updatetime
+                titleage = days_hours_minutes(udur)
+        output += ' %s' % ptime
+        weechat.buffer_set(data, "title", output)
     return weechat.WEECHAT_RC_OK
 
 
@@ -91,7 +138,7 @@ def twitch_buffer_switch(data, signal, signal_data):
     type = weechat.buffer_get_string(signal_data, 'localvar_type')
     if not (server == 'twitch' and type == 'channel'):
         return weechat.WEECHAT_RC_OK
-    twitch_main('', signal_data, '')
+    twitch_main('', signal_data, 'bs')
     return weechat.WEECHAT_RC_OK
 
 
